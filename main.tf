d6296895d7832ec4dd59255901e7d31a323787b4
Ramit Bansal <ramit.bansal@hashicorp.com>
2024-08-13 20:35:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,10 @@
-provider "null" {}
-
-# Resource to validate the TFC_CONFIGURATION_VERSION_REPO_ID environment variable
-resource "null_resource" "validate_repo_name" {
+resource "null_resource" "print_repo_id" {
   provisioner "local-exec" {
-    command = <<EOT
-      if [ -z "$TFC_CONFIGURATION_VERSION_REPO_ID" ]; then
-        echo "TFC_CONFIGURATION_VERSION_REPO_ID is not set."
-        exit 1
-      else
-        echo "TFC_CONFIGURATION_VERSION_REPO_ID is set to: $TFC_CONFIGURATION_VERSION_REPO_ID"
-        # Optionally, you can add a regex check to validate the format
-        if [[ "$TFC_CONFIGURATION_VERSION_REPO_ID" =~ ^[a-zA-Z0-9_-]+/[a-zA-Z0-9_-]+$ ]]; then
-          echo "The repository name format is valid."
-        else
-          echo "The repository name format is invalid."
-          exit 1
-        fi
-      fi
-    EOT
+    command = "echo Repository ID: $TFC_CONFIGURATION_VERSION_REPO_ID"
   }
 }
 
-# Output the value of the TFC_CONFIGURATION_VERSION_REPO_ID environment variable
-output "repository_name" {
-  value = "TFC_CONFIGURATION_VERSION_REPO_ID: ${env.TFC_CONFIGURATION_VERSION_REPO_ID}"
+output "repo_id" {
+  value = terraform.workspace
+  
 }
