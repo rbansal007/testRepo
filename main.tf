a0cf6892da2b30ce399f0275db69af9a9b615b4d
Ramit Bansal <ramit.bansal@hashicorp.com>
2025-05-23 16:08:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "random" {
 }
 
 resource "random_password" "password" {
-  length           = 20
+  length           = 19
   special          = true
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
