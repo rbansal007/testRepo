229fafb415eadcfce091eb1747dd6be7be0be671
Ramit Bansal <ramit.bansal@hashicorp.com>
2025-08-01 16:39:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,34 @@
-terraform {
+terraform { 
+  cloud { 
+    hostname = "ramit-docker.tf-support.hashicorpdemo.com" 
+    organization = "testSupportBundle" 
+    workspaces { 
+      name = "test-ws" 
+    } 
+  } 
   required_providers {
     random = {
       source = "hashicorp/random"
       version = "3.6.3"
     }
   }  
+}
+
+terraform {
+  required_version = ">= 1.3.0"
+  backend "remote" {
+    hostname     = "tfe-01"
+    organization = "yuzhao-terraform"
+    workspaces {
+      name = "demo-workspace"
+    }
+  }
+}
+
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo Hello from TFE!"
+  }
 }
 
 provider "random" {
